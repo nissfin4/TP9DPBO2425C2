b0a7be41591eec01278e1a3718d69e9609cc7621
nissfin4 <nisrina.natun3@upi.edu>
2025-11-25 12:17:26 +0700

    Add files via upload

---
+++ mvp_db.sql
@@ -0,0 +1,115 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 25, 2025 at 06:16 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mvp_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `acara`
+--
+
+CREATE TABLE `acara` (
+  `id` int(11) NOT NULL,
+  `nama_acara` varchar(255) NOT NULL,
+  `lokasi` varchar(255) NOT NULL,
+  `tanggal` date NOT NULL,
+  `jumlah_lap` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `acara`
+--
+
+INSERT INTO `acara` (`id`, `nama_acara`, `lokasi`, `tanggal`, `jumlah_lap`) VALUES
+(1, 'Grand Prix Jakarta', 'Sirkuit Ancol', '2025-03-12', 58),
+(2, 'Night Race Singapore', 'Marina Bay Street Circuit', '2025-04-05', 61),
+(3, 'Japan Speed Festival', 'Suzuka Circuit', '2025-05-18', 53),
+(4, 'Australian Thunder Cup', 'Albert Park Circuit', '2025-06-02', 58),
+(5, 'Italian GP Special Event', 'Monza Circuit', '2025-07-10', 50);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pembalap`
+--
+
+CREATE TABLE `pembalap` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `tim` varchar(255) NOT NULL,
+  `negara` varchar(255) NOT NULL,
+  `poinMusim` int(11) DEFAULT 0,
+  `jumlahMenang` int(11) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `pembalap`
+--
+
+INSERT INTO `pembalap` (`id`, `nama`, `tim`, `negara`, `poinMusim`, `jumlahMenang`) VALUES
+(1, 'Lewis Hamilton ', 'Mercedes', 'United Kingdom', 347, 11),
+(2, 'Max Verstappen', 'Red Bull', 'Netherlands', 335, 10),
+(3, 'Valtteri Bottas', 'Mercedes', 'Finland', 203, 2),
+(4, 'Sergio Perez', 'Red Bull', 'Mexico', 190, 1),
+(5, 'Carlos Sainz', 'Ferrari', 'Spain', 150, 0),
+(6, 'Daniel Ricciardo', 'McLaren', 'Australia', 115, 1),
+(7, 'Charles Leclerc', 'Ferrari', 'Monaco', 95, 0),
+(8, 'Lando Norris', 'McLaren', 'United Kingdom', 88, 0),
+(9, 'Pierre Gasly', 'AlphaTauri', 'France', 75, 0),
+(10, 'Fernando Alonso', 'Alpine', 'Spain', 65, 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `acara`
+--
+ALTER TABLE `acara`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pembalap`
+--
+ALTER TABLE `pembalap`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `acara`
+--
+ALTER TABLE `acara`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `pembalap`
+--
+ALTER TABLE `pembalap`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
